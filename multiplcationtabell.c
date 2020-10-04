81ff2bf4db2e71cf158bb1b96495a78f88c2c1ae
peducation <kaur75606@gmail.com>
2020-10-04 15:38:06 +0200

    Tabel for numbers from 1 to 10

--- multiplcationtabell.c
+++ multiplcationtabell.c
@@ -1,17 +1,18 @@
+ /* Mutiplication tabel for numbers 1 to 10*/
  #include <stdio.h>
  #include <math.h>
 
  int mul_tabel(int n){
-     for(int i = 1; i <=10; i++){
+     for(int i = 1; i <=10; i++){      // Loop to generate tabel from 1 to 10
          int res = i*n;
-         printf("%d   %d\n", i, res);
+         printf("%d   %d\n", i, res);   // display number 1 to 10 and given number's factors
      }
  }
 
  int main(){
      int number, mul;
-     printf("Enter any one number from 1 to 10: ");
+     printf("Enter any one number from 1 to 10: ");  // take any number but from 1 to 10 only
      scanf("%d", &number);
-     mul = mul_tabel(number);
+     mul = mul_tabel(number);   // calling to function with function name
      printf(mul);
       }
