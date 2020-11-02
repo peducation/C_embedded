006d9225d2b9ed10a894c130396a8c88e2d04790
peducation <kaur75606@gmail.com>
2020-11-02 11:10:09 +0100

    First c++ program

---
+++ arithmaticoperatorEx2-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int value = 10;
+    cout << value <<endl;
+    value *=2;
+    cout << value-- <<endl;
+    //value--;
+    cout << value <<endl;
+
+    return 0;
+}
