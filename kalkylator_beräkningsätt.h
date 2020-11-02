a85e7d58556d45562919a2a793584cdb2c55ee88
peducation <kaur75606@gmail.com>
2020-11-02 11:05:44 +0100

    Examen uppgifter 4

---
+++ kalkylator_beräkningsätt.h
@@ -0,0 +1,12 @@
+//Problem4Kalkylator
+/*Task is to create small calculator which will do calculations with four
+different methods. It also includes to make functions in separate file*/
+//Created by Navjot Kaur
+//Date: 2020-10-28
+
+//Define the functions
+float sum(float num1, float num2);
+float subtract(float num1, float num2);
+float mul(float num1, float num2);
+float division(float num1, float num2);
+float result(float num1, float num2);
