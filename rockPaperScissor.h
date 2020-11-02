7fe140cd111ef19df3a96118513cdf3042ff3130
peducation <kaur75606@gmail.com>
2020-11-02 11:07:50 +0100

    Examen uppgifter 5

---
+++ rockPaperScissor.h
@@ -0,0 +1,10 @@
+//Problem5StenSaxPåse
+/*This program is to make game called as Rock, Paper and Scissor
+where 0 is represented as Rock, 1 as Scissor and 2 as Paper.*/
+// Created by Navjot Kaur
+//Date: 2020-10-31
+
+//Define the functions
+int user_input();
+int computer_input();
+int rock_scissor_paper();
